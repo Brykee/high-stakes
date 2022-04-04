9f2d24535eddd527cdc3bab892c79a44bf5af7a2
Bryce Richardson <drichardson2@harding.edu>
2022-04-04 15:25:56 -0500

    shuffled cards my own way

--- src/App.js
+++ src/App.js
@@ -1,18 +1,88 @@
 import React from "react";
 import "./App.css";
 import Cards from "./components/Cards";
+import cardback from "../src/pics/lesscreepycardback.jpg";
+
+const jester = {
+  num: 1,
+  img: cardback,
+};
+
+const two = {
+  num: 2,
+  img: cardback,
+};
+const three = {
+  num: 3,
+  img: cardback,
+};
+const four = {
+  num: 4,
+  img: cardback,
+};
+const five = {
+  num: 5,
+  img: cardback,
+};
+const six = {
+  num: 6,
+  img: cardback,
+};
+const seven = {
+  num: 7,
+  img: cardback,
+};
+const eight = {
+  num: 8,
+  img: cardback,
+};
+const nine = {
+  num: 9,
+  img: cardback,
+};
 
 class App extends React.Component {
   render() {
-    function renderGame() {}
+    const cardsArray = [
+      jester,
+      two,
+      three,
+      four,
+      five,
+      six,
+      seven,
+      eight,
+      nine,
+    ];
 
-    return (
-      <div className="table">
-        <button className="startBtn" onClick={renderGame}>
-          Start Game
-        </button>
-      </div>
-    );
+    function shuffle(array) {
+      let currentIndex = array.length,
+        randomIndex;
+
+      // While there remain elements to shuffle...
+      while (currentIndex != 0) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ];
+      }
+
+      return array;
+    }
+
+    const cardsElements = shuffle(cardsArray).map((card) => (
+      <a>
+        <img className="cardPic" src={cardback} alt="cardback" />
+        {card.num}
+      </a>
+    ));
+
+    return <div className="table">{cardsElements}</div>;
   }
 }
 
