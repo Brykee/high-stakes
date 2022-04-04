2c9de1d7bbccbe62b3d2d4f5deb563788a051338
Bryce Richardson <drichardson2@harding.edu>
2022-04-04 18:16:16 -0500

    random shuffle unique cards

--- src/App.js
+++ src/App.js
@@ -1,47 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Cards from "./components/Cards";
 import cardback from "../src/pics/lesscreepycardback.jpg";
+import jesterCard from "../src/pics/jokerCard.jpg";
+import card2 from "../src/pics/2card.jpg";
+import card3 from "../src/pics/3card.jpg";
+import card4 from "../src/pics/4card.jpg";
+import card5 from "../src/pics/5card.jpg";
+import card6 from "../src/pics/6card.jpg";
+import card7 from "../src/pics/7card.jpg";
+import card8 from "../src/pics/8card.jpg";
+import card9 from "../src/pics/9card.jpg";
 
 const jester = {
   num: 1,
-  img: cardback,
+  img: jesterCard,
 };
 
 const two = {
   num: 2,
-  img: cardback,
+  img: card2,
 };
 const three = {
   num: 3,
-  img: cardback,
+  img: card3,
 };
 const four = {
   num: 4,
-  img: cardback,
+  img: card4,
 };
 const five = {
   num: 5,
-  img: cardback,
+  img: card5,
 };
 const six = {
   num: 6,
-  img: cardback,
+  img: card6,
 };
 const seven = {
   num: 7,
-  img: cardback,
+  img: card7,
 };
 const eight = {
   num: 8,
-  img: cardback,
+  img: card8,
 };
 const nine = {
   num: 9,
-  img: cardback,
+  img: card9,
 };
 
 class App extends React.Component {
+  // const [cards, setCards] = useState([]);
+  // const [turns, setTurns] = useState(0);
+
   render() {
     const cardsArray = [
       jester,
@@ -75,10 +86,11 @@ class App extends React.Component {
       return array;
     }
 
+    function flip() {}
+
     const cardsElements = shuffle(cardsArray).map((card) => (
       <a>
-        <img className="cardPic" src={cardback} alt="cardback" />
-        {card.num}
+        <img className="cardPic" src={card.img} alt="cardback" />
       </a>
     ));
 
@@ -87,3 +99,8 @@ class App extends React.Component {
 }
 
 export default App;
+
+{
+  /* <img className="front" src={card.img} alt="card front" />
+                <img className="back" src={cardback} alt="card back" /> */
+}
